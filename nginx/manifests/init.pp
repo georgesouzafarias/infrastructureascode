7e929d1fe76f9c5093ac28680a0a5b89b0283493
George Souza Farias <georgesouzafarias@gmail.com>
2019-11-21 22:21:04 -0300

    declared the newest classes.

--- nginx/manifests/init.pp
+++ nginx/manifests/init.pp
@@ -6,4 +6,10 @@
 #   include nginx
 class nginx {
   contain nginx::install
+  contain nginx::config
+  contain nginx::service
+
+  Class['nginx::install']
+  -> Class['nginx::config']
+  ~> Class['nginx::service']
 }
